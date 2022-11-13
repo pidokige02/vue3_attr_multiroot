61aa4024a03c7d0d1e1bc96074c8f8b8a744364c
pidokige02 <pidokige0204@gmail.com>
2022-11-13 16:30:43 +0900

    code submitted as final one

---
+++ src/components/BaseInput.vue
@@ -0,0 +1,29 @@
+<template>
+      <label>{{ label }}</label>
+      <input
+        v-bind="{
+          ...$attrs,
+          onInput: (event) => $emit('update:modelValue', event.target.value)
+        }"
+        :value="modelValue"
+      />
+</template>
+
+  <script>
+  export default {
+    inheritAttrs: false,
+    props: {
+      modelValue: {
+        type: [String, Number],
+        default: ''
+      },
+      label: {
+        type: String,
+        default: null
+      }
+    },
+    setup (props, context) {
+      console.log(context.attrs)
+    }
+  }
+  </script>
